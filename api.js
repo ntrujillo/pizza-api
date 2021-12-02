c0ff88ba11461896f94f3faf3acc385547a1d4a4
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2021-12-02 00:30:09 -0500

    add external api

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const getPizzas = require('./handlers/get-pizzas');
 const createOrder = require('./handlers/create-order');
 const deleteOrder = require('./handlers/delete-order');
 const updateOrder = require('./handlers/update-order');
+const updateDeliveryStatus = require('./handlers/update-delivery-status');
 
 api.get('/', () => 'Welcome to Pizza Api');
 api.get('/pizzas', () => getPizzas());
@@ -20,4 +21,9 @@ api.put('/orders/{id}', request => updateOrder(request.pathParams.id, request.bo
 api.delete('/orders/{id}', request => deleteOrder(request.pathParams.id), {
     error: 400
 });
+api.post('/delivery', request => updateDeliveryStatus(request.body), {
+    success: 200,
+    error: 400
+});
+
 module.exports = api;
--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,20 +1,34 @@
-const AWS = require('aws-sdk')
-const docClient = new AWS.DynamoDB.DocumentClient()
-const uuid = require('uuid').v4;
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const rp = require('minimal-request-promise');
 
 function createOrder(request) {
     if (!request || !request.pizza || !request.address)
         throw new Error('To order pizza please provide pizza type and address where pizza should be delivered')
 
-    return docClient.put({
-        TableName: 'pizza-orders',
-        Item: {
-            orderId: uuid(),
-            pizza: request.pizza,
-            address: request.address,
-            orderStatus: 'pending'
-        }
-    }).promise()
+    return rp.post('https://some-like-it-hot.effortless-serverless.com/delivery', {
+        headers: {
+            "Authorization": "aunt-marias-pizzeria-1234567890",
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            pickupTime: '15.34pm',
+            pickupAddress: 'Aunt Maria Pizzeria',
+            deliveryAddress: request.address,
+            webhookUrl: 'https://whpcvzntil.execute-api.eu-central-1.amazonaws.com/chapter4_1/delivery'
+        })
+    }).then(rawResponse => JSON.parse(rawResponse.body))
+        .then(response => {
+            return docClient.put({
+                TableName: 'pizza-orders',
+                Item: {
+                    orderId: response.deliveryId,
+                    pizza: request.pizza,
+                    address: request.address,
+                    orderStatus: 'pending'
+                }
+            }).promise()
+        })
         .then((res) => {
             console.log('Order is saved!', res)
             return res
